065c0c645f74cf03cf4aebd562539affe15a389e
Riya Samantaray <riyasamantaray23@gmail.com>
2025-07-03 11:40:48 +0530

    changed json for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const dotenv =require('dotenv');
 const cors = require('cors');
 
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production'
+        ? 'YOUR_FRONTEND_RENDER_URL' // You'll fill this in later
+        : 'http://localhost:5173' // Or whatever your local frontend port is
+}));
+
 dotenv.config();
 console.log('DEBUG: MONGO_URI from process.env:', process.env.MONGO_URI);
 
